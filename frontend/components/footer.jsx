2c1b36ed174c71dbf83f61fb9017e061b5384dc0
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-07-04 00:08:58 +0000

    working

---
+++ frontend/components/footer.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export const Footer = () => (
+  <footer className="footer mt-auto text-center" style={{ color: "#28549f" }}>
+    <p>
+      Made with <i className="fa fa-heart text-danger" /> by{" "}
+      <a href="https://github.com/taylor-allen">Taylor Allen</a>
+    </p>
+  </footer>
+);
---
+++ frontend/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AddContact } from "../components/addContact.jsx";
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-container">
+      <Link to="/" style={{ color: "transparent" }}>
+        <h3>Taylor's Contact List</h3>
+      </Link>
+      <div className="ml-auto">
+        <Link to="/AddContact" className="btn btn-success navbar-button">
+          Add New Contact
+        </Link>
+      </div>
+    </nav>
+  );
+};

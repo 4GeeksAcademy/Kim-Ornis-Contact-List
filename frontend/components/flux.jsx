0e1f42afdf7864f6042e5d25f9bbf07b7dc4fadc
KOTHECODE <160642762+KOTHECODE@users.noreply.github.com>
2024-07-09 01:05:27 +0000

    flux added

---
+++ frontend/components/flux.jsx
@@ -0,0 +1,109 @@
+const getState = ({ getStore, getActions, setStore }) => {
+    return {
+      store: {
+        demo: [
+          {
+            title: "FIRST",
+            background: "white",
+            initial: "white",
+          },
+          {
+            title: "SECOND",
+            background: "white",
+            initial: "white",
+          },
+        ],
+        contacts: [],
+      },
+      actions: {
+        getContacts: () => {
+          fetch(
+            "https://playground.4geeks.com/apis/fake/contact/agenda/taylor-allen"
+          )
+            .then((resp) => {
+              if (!resp.ok) throw Error(resp.statusText);
+              return resp.json();
+            })
+            .then((data) => {
+              console.log(data);
+  
+              // Set the retrieved contacts data in the store
+              setStore({ contacts: data });
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+  
+        addContacts: (contactData) => {
+          const url = "https://playground.4geeks.com/apis/fake/contact/";
+          const request = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(contactData),
+          };
+  
+          fetch(url, request)
+            .then((resp) => {
+              if (!resp.ok) throw Error(resp.statusText);
+              return resp.json();
+            })
+            .then((data) => {
+              console.log(data);
+              getActions().getContacts();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+  
+        deleteContacts: (id) => {
+          const url = `https://playground.4geeks.com/apis/fake/contact/${id}`;
+          const request = {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          };
+  
+          fetch(url, request)
+            .then((resp) => {
+              if (!resp.ok) throw Error(resp.statusText);
+              return resp.json();
+            })
+            .then((data) => {
+              console.log(data);
+              getActions().getContacts();
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+  
+        editContact: (id, contactData) => {
+          fetch(`https://playground.4geeks.com/apis/fake/contact/${id}`, {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(contactData),
+          })
+            .then((resp) => {
+              if (!resp.ok) throw Error(resp.statusText);
+              return resp.json();
+            })
+            .then((data) => {
+              console.log(data);
+              getActions().getContacts();
+            })
+            .catch((error) => {
+              console.error("Error", error);
+            });
+        },
+      },
+    };
+  };
+  
+  export default getState;
